d33d26b197dd0831062584e092725d03dadb311d
clarencypasaribu <clarencypasaribu@gmail.com>
2025-04-09 23:08:59 +0700

    Create Petani.java

---
+++ pertemuan 5/latihan/Petani.java
@@ -0,0 +1,17 @@
+// Kelas Petani
+class Petani extends Manusia {
+    private String asal_kota;
+    private static int counterPetani;
+    private final int C = 0;
+
+    public Petani(String nama, LocalDate tgl_mulai_kerja, String alamat, double pendapatan, String asal_kota) {
+        super(nama, tgl_mulai_kerja, alamat, pendapatan);
+        this.asal_kota = asal_kota;
+        counterPetani++;
+    }
+
+    @Override
+    public int hitungMasaKerja() {
+        return (int) ChronoUnit.YEARS.between(tgl_mulai_kerja, LocalDate.now()) + C;
+    }
+}
